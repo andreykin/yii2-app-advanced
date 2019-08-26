8b1e1a954a88cbc1f5c6d6673e2dfd6424e4ced5
andreykin <mail@andreyko.ru>
2019-08-26 18:59:28 +0300

    fix issue "/usr/bin/env: php : No such file or directory" with vagrant provision script

--- vagrant/provision/once-as-vagrant.sh
+++ vagrant/provision/once-as-vagrant.sh
@@ -22,8 +22,8 @@ info "Init project"
 php init --env=Development --overwrite=y
 
 info "Apply migrations"
-./yii migrate --interactive=0
-./yii_test migrate --interactive=0
+php yii migrate --interactive=0
+php yii_test migrate --interactive=0
 
 info "Create bash-alias 'app' for vagrant user"
 echo 'alias app="cd /app"' | tee /home/vagrant/.bash_aliases
